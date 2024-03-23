6d2493d67889c9c3f3d671480cef48df727a650a
rajnishsubedi <rajnishsubedi0@gmail.com>
2024-03-23 13:33:57 +0545

    .

---
+++ app/src/main/java/com/example/downloadFromUrl/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.downloadFromUrl;
+
+import android.app.DownloadManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.webkit.CookieManager;
+import android.webkit.URLUtil;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        String url="https://raw.githubusercontent.com/rajnishsubedi0/UrlToDownload/main/app-release.apk";
+        DownloadManager.Request dwnldRequest=new DownloadManager.Request(Uri.parse(url));
+        String title= URLUtil.guessFileName(url,null,null);
+        dwnldRequest.setTitle(title);
+        dwnldRequest.setDescription("Downloading");
+        String cookie= CookieManager.getInstance().getCookie(url);
+        dwnldRequest.addRequestHeader("cookie",cookie);
+        dwnldRequest.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+        dwnldRequest.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS,title);
+        DownloadManager downloadManager=(DownloadManager) getSystemService(DOWNLOAD_SERVICE);
+        downloadManager.enqueue(dwnldRequest);
+        Toast.makeText(this, "DownloadingStarted", Toast.LENGTH_SHORT).show();
+    }
+}
